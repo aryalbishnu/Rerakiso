77aa79b8665cbab3fb79a7b2ba583422eb40e1ca
aryalbishnu <ariyalbishnu@gmail.com>
2022-05-10 18:18:32 +0900

    creat a model

---
+++ src/main/java/com/example/demo/model/StudentInfo.java
@@ -0,0 +1,29 @@
+package com.example.demo.model;
+
+import lombok.Data;
+
+@Data
+public class StudentInfo {
+	private String name;
+	private String country;
+	private String gender;
+	private String birthday;
+	private String age;
+	private String school;
+	private String subject;
+	private String graduation;
+	private String address;
+	private String station;
+	private String qualification;
+	private String duration;
+	private Integer month;
+	private String system;
+	private String topic;
+	private String os;
+	private String programing;
+	
+	
+	
+	
+
+}
